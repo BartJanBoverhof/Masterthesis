a888a864a9be792714fe48b5d805247dcf876a20
BartJanBoverhof <bjboverhof@gmail.com>
2021-01-20 13:07:01 +0100

    Added some usefull packages in python scrips

--- 3.main_analysis/singe_modular/main.py
+++ 3.main_analysis/singe_modular/main.py
@@ -1,9 +1,15 @@
+"""
+Single-modular deep neural network
+"""
+
 ################### 0. Prerequisites ###################
 
 #Loading packages
-import torch
+import torch #PyTorch deep-learning library
+from torch import nn #PyTorch additionals
+from torch import optim #PyTorch optimizer for network training
+import torch.nn.functional as F #PyTorch pre specified function library
 
 
-x = torch.rand(5,3)
+x = torch.randn(10, 10)
 print(x)
-
