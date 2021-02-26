a520cc2267eefff53e7fab87fa796e7367c1bfcc
BartJanBoverhof <bjboverhof@gmail.com>
2021-02-26 18:10:58 +0100

    Changes
    - Completed EEG CovNet
    - Added k-fold cross-validation

--- pytorch/networks.py
+++ pytorch/networks.py
@@ -23,24 +23,23 @@ class EEGNet(nn.Module):
         self.drop = drop
 
         #Convolutional layers
-        self.conv1 = nn.Conv2d(in_channels = 1, out_channels = 25, kernel_size = 3, padding=1)
-        self.conv2 = nn.Conv2d(in_channels = 25, out_channels = 50, kernel_size = 3, padding=1)
-        self.conv3 = nn.Conv2d(in_channels = 50, out_channels = 100, kernel_size = 3, padding=1)
-        self.conv4 = nn.Conv2d(in_channels = 100, out_channels = 200, kernel_size = 3, padding=1)
+        self.conv1 = nn.Conv1d(in_channels = 4, out_channels = 25, kernel_size = 3, padding=1)
+        self.conv2 = nn.Conv1d(in_channels = 25, out_channels = 50, kernel_size = 3, padding=1)
+        self.conv3 = nn.Conv1d(in_channels = 50, out_channels = 100, kernel_size = 3, padding=1)
+        self.conv4 = nn.Conv1d(in_channels = 100, out_channels = 200, kernel_size = 3, padding=1)
 
         #Max pooling layer (3x1)
-        self.pool = nn.MaxPool2d(kernel_size = 1, stride = (1,3)) 
+        self.pool = nn.MaxPool1d(kernel_size = 3, stride = 3) 
 
         #Batch normalization
-        self.batch1 = nn.BatchNorm2d(num_features = 25)
-        self.batch2 = nn.BatchNorm2d(num_features = 50)
-        self.batch3 = nn.BatchNorm2d(num_features = 100)
-        self.batch4 = nn.BatchNorm2d(num_features = 200)
+        self.batch1 = nn.BatchNorm1d(num_features = 25)
+        self.batch2 = nn.BatchNorm1d(num_features = 50)
+        self.batch3 = nn.BatchNorm1d(num_features = 100)
+        self.batch4 = nn.BatchNorm1d(num_features = 200)
 
         #Dense layer
-        self.dense1 = nn.Linear(20800,2000) 
-        self.dense2 = nn.Linear(2000,250) 
-        self.dense3 = nn.Linear(250,1) 
+        self.dense1 = nn.Linear(3000,400) 
+        self.dense2 = nn.Linear(400,1) 
 
         self.dropout = nn.Dropout(0.25)
 
@@ -51,12 +50,10 @@ class EEGNet(nn.Module):
         x = self.pool(F.elu(self.batch3(self.conv3(x)))) #Third block
         x = self.pool(F.elu(self.batch4(self.conv4(x)))) #Fourth block
         
-        x = x.view(-1, x.shape[1]* x.shape[2]* x.shape[3]) #Flatten
+        x = x.view(-1, x.shape[1]* x.shape[2]) #Flatten
         x = self.dropout(x)
         x = F.relu(self.dense1(x))
-        x = self.dropout(x)
-        x = F.relu(self.dense2(x))
-        x = self.dense3(x)
+        x = self.dense2(x)
 
         return x
 
@@ -135,34 +132,67 @@ class PPGNet(nn.Module):
 
 ################### GSR Net ###################
 class GSRNet(nn.Module):
-    def __init__(self):
-        super(GsrNet, self).__init__()
-        #Convolutional layers
-        self.conv1 = nn.Conv1d(3, 16, 3, padding=1)
-        self.conv1 = nn.Conv1d(3, 16, 3, padding=1)
 
-        #Max pooling layer (3x1)
-        self.pool = nn.MaxPool1d(kernel_size = 4, stride = 1) 
+    def __init__(self, filters , hidden_dim, n_layers =2, drop = 0.1):
+        super(GSRNet, self).__init__()
+        
+        self.ip_dim = filters
+        self.hidden_dim = hidden_dim
+        self.n_layers = n_layers
+        self.drop = drop
+
+        #Convolutional layers
+        self.conv1 = nn.Conv1d(in_channels =  1, out_channels = filters, kernel_size = 3, padding=1)
+        self.conv2 = nn.Conv1d(in_channels = filters, out_channels = filters, kernel_size = 3, padding=1)
+
+        #Max pooling layer (4)
+        self.pool = nn.MaxPool1d(kernel_size = 4, stride = 4) 
 
         #Batch normalization
-        self.batch1 = nn.BatchNorm1d(num_features = 1)
-        self.batch2 = nn.BatchNorm1d(num_features = 1)
+        self.batch1 = nn.BatchNorm1d(num_features = filters)
+        self.batch2 = nn.BatchNorm1d(num_features = filters)
+        self.batch3 = nn.BatchNorm1d(num_features = int(hidden_dim/4))
 
-        #LSTM layers
-        self.lstm = nn.LSTM(10)
+        #Dense & classification layer 
+        self.fc1 = nn.Linear(hidden_dim, int(hidden_dim/4))
+        self.fc2 = nn.Linear(int(hidden_dim/4), 1)
 
-        #Dense layer
-        self.dense = nn.Linear(100,10) 
+
+        #LSTM layer
+        self.lstm = nn.LSTM(input_size = filters, 
+                                hidden_size = hidden_dim, 
+                                num_layers = n_layers, 
+                                dropout = drop,
+                                batch_first= True)
+    
+    def InitHiddenstate(self, batch_size):
+           
+        weight = next(self.parameters()).data
+        hidden = (weight.new(self.n_layers, batch_size, self.hidden_dim).zero_(),
+                  weight.new(self.n_layers, batch_size, self.hidden_dim).zero_())
         
-        
-    def forward(self, x): 
+        return hidden
+
+    def forward(self, x, hidden): 
+
+        batch_size = x.shape[0]
+        #Convolutional block
         x = self.pool(F.relu(self.batch1(self.conv1(x)))) #First block
         x = self.pool(F.relu(self.batch2(self.conv2(x)))) #Second block
-        x = self.lstm(x) #Third block
-        x = self.lstm(x) #Fourth block
-        x = F.softmax(self.dense(x)) #Classification block
-        return x
+        
+        #LSTM-block
+        x = torch.transpose(x, 1, 2)
+        out, hidden = self.lstm(x, hidden)
+        out = out.contiguous().view(-1, self.hidden_dim)
 
+        #Prediction block
+        out = F.relu(self.batch3(self.fc1(out)))
+        out = self.fc2(out)
+
+        out = out.view(batch_size, -1)
+        out = out[:,-1]
+
+        return out, hidden
 #Display network
 #GsrNet()
 
